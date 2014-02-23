03bc24d3938564e7863f5f266558bae62a00f043
Devon Page <devon.page@live.com>
2014-02-23 13:38:27 +0000

    Initial release
    Basic files to get started

---
+++ header.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Seabird</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="shortcut icon" type="image/x-icon" href="style/images/favicon.png" />
+<link href="style/css/bootstrap.css" rel="stylesheet">
+<link href="style/css/settings.css" rel="stylesheet">
+<link href="style/js/google-code-prettify/prettify.css" rel="stylesheet">
+<link href="style/js/fancybox/jquery.fancybox.css" rel="stylesheet" type="text/css" media="all" />
+<link href="style/js/fancybox/helpers/jquery.fancybox-thumbs0ff5.css?v=1.0.2" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet">
+<link href="style/css/color/green.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700' rel='stylesheet' type='text/css'>
+<link href="style/type/fontello.css" rel="stylesheet">
+<!--[if IE 8]>
+<link rel="stylesheet" type="text/css" href="style/css/ie8.css" media="all" />
+<![endif]-->
+<!--[if lt IE 9]>
+<script src="style/js/html5shiv.js"></script>
+<![endif]-->
+</head>
+<body>
+<div class="box box-border">
+  <div class="light-wrapper">
+    <div class="container">
+      <header>
+        <div class="logo pull-left"> <a href="index.html"><img src="style/images/logo.png" alt="" /></a> </div>
+        <ul class="contact-info pull-right">
+          <li><i class="icon-mail"></i><a href="mailto:#">first.last@email.com</a></li>
+          <li><i class="icon-phone-1"></i>+00 (123) 456 78 90</li>
+        </ul>
+      </header>
+      <!-- /header -->
+      <div class="menu-wrapper">
+        <nav id="menu" class="menu">
+          <ul id="tiny">
+            <li class="active"><a href="index.html">Home</a>
+              <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="index2.html">Home II</a></li>
+                <li><a href="index3.html">Home III</a></li>
+                <li><a href="index4.html">Home IV</a></li>
+              </ul>
+            </li>
+            <li><a href="portfolio.html">Portfolio</a>
+              <ul>
+                <li><a href="portfolio.html">Portfolio</a></li>
+                <li><a href="portfolio-post.html">Post with Slider</a></li>
+                <li><a href="portfolio-post2.html">Post with Images</a></li>
+                <li><a href="portfolio-post3.html">Post with Video</a></li>
+              </ul>
+            </li>
+            <li><a href="about.html">Pages</a>
+              <ul>
+                <li><a href="about.html">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="page-with-sidebar.html">Page with Sidebar</a></li>
+              </ul>
+            </li>
+            <li><a href="blog.html">Blog</a>
+              <ul>
+                <li><a href="blog.html">Grid Blog with Sidebar</a></li>
+                <li><a href="blog2.html">Grid Blog without Sidebar</a></li>
+                <li><a href="blog3.html">Blog Medium</a></li>
+                <li><a href="blog4.html">Blog Large</a></li>
+                <li><a href="blog-post.html">Blog Post</a></li>
+              </ul>
+            </li>
+            <li><a href="elements.html">Features</a>
+              <ul>
+                <li><a href="#">Color</a>
+                <ul>
+	              <li><a class="changecolor" title="blue-color">Blue</a></li>
+	              <li><a class="changecolor" title="green-color">Green</a></li>
+	              <li><a class="changecolor" title="orange-color">Orange</a></li>
+	              <li><a class="changecolor" title="pink-color">Pink</a></li>
+	              <li><a class="changecolor" title="purple-color">Purple</a></li>
+	              <li><a class="changecolor" title="rose-color">Rose</a></li>
+	            </ul>
+              </li>
+              <li><a href="#">Background</a>
+                <ul>
+	              <li><a class="changebg" title="bg1-bg">Background 1</a></li>
+	              <li><a class="changebg" title="bg2-bg">Background 2</a></li>
+	              <li><a class="changebg" title="bg3-bg">Background 3</a></li>
+	              <li><a class="changebg" title="bg4-bg">Background 4</a></li>
+	              <li><a class="changebg" title="bg5-bg">Background 5</a></li>
+	              <li><a class="changebg" title="bg6-bg">Background 6</a></li>
+	            </ul>
+              </li>
+          <li><a href="elements.html">Elements</a></li>
+                <li><a href="pricing-table.html">Pricing Table</a></li>
+                <li><a href="retina-icons.html">Retina Icons</a></li>
+              </ul>
+            </li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
+        <!-- /.menu -->
+        <ul class="social">
+          <li><a href="#"><i class="icon-s-rss"></i></a></li>
+          <li><a href="#"><i class="icon-s-twitter"></i></a></li>
+          <li><a href="#"><i class="icon-s-facebook"></i></a></li>
+        </ul>
+        <!-- /.social --> 
+      </div>
+      <!-- /.menu-wrapper -->
+</body>
+</html>
